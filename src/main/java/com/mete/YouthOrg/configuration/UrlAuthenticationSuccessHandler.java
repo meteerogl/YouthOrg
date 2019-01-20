8d4edc3a4026fa9c01ae0969beb8b656cdfa2f62
meteerogl <meteerogl@gmail.com>
2019-01-20 21:00:04 +0300

    data script

--- src/main/java/com/mete/YouthOrg/configuration/UrlAuthenticationSuccessHandler.java
+++ src/main/java/com/mete/YouthOrg/configuration/UrlAuthenticationSuccessHandler.java
@@ -7,11 +7,18 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+
+/*
+* Admin can manage all works
+* Organization Admin is responsible just her/his organization.He/She can add university representer
+* University Representer is responsible just own university.He/She can add users
+* Users is users
+*/
 
 @Component
 public class UrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
@@ -23,9 +30,7 @@ public class UrlAuthenticationSuccessHandler implements AuthenticationSuccessHan
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
         //set our response to OK status
         httpServletResponse.setStatus(httpServletResponse.SC_OK);
-
         logger.info("at OnAuthenticationSuccess(..) function");
-        boolean admin =false;
 
         for(GrantedAuthority auth: authentication.getAuthorities()){
             if("ADMIN".equals(auth.getAuthority())){
@@ -40,9 +45,6 @@ public class UrlAuthenticationSuccessHandler implements AuthenticationSuccessHan
             else if("USER".equals(auth.getAuthority())){
                 httpServletResponse.sendRedirect("user/home");
             }
-
-
         }
-
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,18 @@
-INSERT INTO `universities` (`uni_id`, `uni_name`) VALUES ('1', 'YILDIRİM');
+--------
+INSERT INTO `countries` (`country_id`,`country_name`) VALUES ('1','TURKIYE');
+INSERT INTO `countries` (`country_id`,`country_name`) VALUES ('2','ESTONIA');
+--------
+INSERT INTO `provinces` (`province_id`,`province_name`,`country_country_id`) VALUES ('1','ADANA','1');
+INSERT INTO `provinces` (`province_id`,`province_name`,`country_country_id`) VALUES ('2','ANKARA','1');
+--------
+INSERT INTO `universities` (`uni_id`, `uni_name`,`uni_country_country_id`,`uni_province_province_id`) VALUES ('1', 'YILDIRIM BEYAZIT UNIVERSITESI','1','2');
+INSERT INTO `universities` (`uni_id`, `uni_name`,`uni_country_country_id`,`uni_province_province_id`) VALUES ('2', 'ODTU','1','2');
+--------
+INSERT INTO `organizations` (`org_id`,`org_name`,`org_detail`) VALUES ('1','BCYCLE LOVERS','BCYCLE IS LIFE');
+INSERT INTO `organizations` (`org_id`,`org_name`,`org_detail`) VALUES ('2','BOOK LOVERS','BOOK IS PART OF LIFE');
+--------
+INSERT INTO `role` (`role_id`,`role`) VALUES ('1','ADMIN');
+INSERT INTO `role` (`role_id`,`role`) VALUES ('2','ORGADMIN');
+INSERT INTO `role` (`role_id`,`role`) VALUES ('3','UNIREP');
+INSERT INTO `role` (`role_id`,`role`) VALUES ('4','USER');
+
